f0e2e7bf0a369f78e98418ba8416afe77ac2870f
Ky <ky@MacBook-Pro-2.local>
2022-03-08 04:02:59 +0100

    Add base ui templates, implement basic routing and redirect for root path, install search app in settings

--- dle/search/models.py
+++ dle/search/models.py
@@ -1,3 +1,8 @@
+from typing import NamedTuple
 from django.db import models
 
 # Create your models here.
+class MockLabel(NamedTuple):
+    manufacturer: str
+    name: str
+    label_text: str
--- dle/search/views.py
+++ dle/search/views.py
@@ -1,3 +1,11 @@
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+)
 from django.shortcuts import render
+from .search_mock_utils import SEARCH_RESULTS
+
 
 # Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, "search/search.html")
