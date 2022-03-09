ac33df1d6da504b8fe93c84f2c9104c72b5614f5
Ky <ky@MacBook-Pro-2.local>
2022-03-09 04:58:06 +0100

    Refine search form, add search results page

--- dle/search/views.py
+++ dle/search/views.py
@@ -1,6 +1,6 @@
 from django.http import (
     HttpRequest,
-    HttpResponse,
+    HttpResponse
 )
 from django.shortcuts import render
 from .search_mock_utils import SEARCH_RESULTS
@@ -9,3 +9,11 @@ from .search_mock_utils import SEARCH_RESULTS
 # Create your views here.
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "search/search.html")
+
+def list_search_results(request: HttpRequest) -> HttpResponse:
+    print(request.method)
+    print(request.body)
+    context = {
+        'search_results': SEARCH_RESULTS
+    }
+    return render(request, "search/search_results.html", context=context)
