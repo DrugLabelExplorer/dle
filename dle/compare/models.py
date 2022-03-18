f4da32aab36137bbe77f8573e0dd7fc319bf1f2e
Sam Negassi <samson.s.negassi@gmail.com>
2022-03-18 09:30:21 -0700

    added DrugLabel class in models

--- dle/compare/models.py
+++ dle/compare/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class DrugLabel(models.Model):
+    label_id = models.CharField(max_length=200)
+    label_name = models.CharField(max_length=200)
+    manufacturer = models.CharField(max_length=200)
+    indication_usage = models.TextField()
+    dosage = models.TextField()
+    contraindication = models.TextField()
+    adverse_reaction = models.TextField()
+    description = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
