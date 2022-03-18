75ff4ec40245286710bdd2d84c81bdb38208d645
Sam Negassi <samson.s.negassi@gmail.com>
2022-03-18 11:21:41 -0700

    added compare, compare_all routs

--- dle/compare/urls.py
+++ dle/compare/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'compare'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/compare_all', views.compare_all, name='compare_all'),
 ]
