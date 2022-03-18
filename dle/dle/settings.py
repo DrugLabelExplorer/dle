a4d4038fd5e4a9f59f1349a531bfa953ff31ce33
Sam Negassi <samson.s.negassi@gmail.com>
2022-03-18 08:37:21 -0700

    update settings.py with compare app

--- dle/dle/settings.py
+++ dle/dle/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'compare.apps.CompareConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
