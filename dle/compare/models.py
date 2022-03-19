1d3bf99b47fe32b4b6a4fe86e51d03e138ea4761
Sam Negassi <samson.s.negassi@gmail.com>
2022-03-19 12:11:28 -0700

    move sec dict to models, update str #4

--- dle/compare/models.py
+++ dle/compare/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
 
+# Section field name vs frontend display name
+SEC_DISPLAY_NAMES = {
+    'indication_usage': "Indication & Usage",
+    'dosage': "Dosage & Administration",
+    'contraindication': 'Contraindication',
+    'adverse_reaction': 'Adverse Reaction',
+    'description': 'Description'
+}
+
 class DrugLabel(models.Model):
+    """Mock-up drug label data model
+    """
     label_id = models.CharField(max_length=200)
     label_name = models.CharField(max_length=200)
     manufacturer = models.CharField(max_length=200)
@@ -12,5 +23,5 @@ class DrugLabel(models.Model):
     description = models.TextField()
 
     def __str__(self):
-        return f"Label ID: {self.label_id}\nManufacturer: {self.manufacturer}"
+        return f"Label ID: {self.label_id}, Manufacturer: {self.manufacturer}"
 
