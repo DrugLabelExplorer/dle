62ca3308f01e225e265925ea27e1fca0151b9cbf
agi-codes <agk547@g.harvard.edu>
2022-04-30 18:23:53 -0400

    nav adn styling

--- dle/users/urls.py
+++ dle/users/urls.py
@@ -3,6 +3,8 @@ from . import views
 from dle import settings
 from django.conf.urls.static import static
 
+app_name = "users"
+
 urlpatterns = [
     # url pattern referencing workflow of overall app
     path("", views.index, name="index"),
@@ -12,7 +14,10 @@ urlpatterns = [
     path('myqueries/', views.myqueries, name='myqueries'),
 
     path("profile/<int:userid>", views.profile, name="profile"),
-    path("save_toggle/<int:postid>", views.save_toggle, name="save_toggle")
+    path("save_toggle/<int:postid>", views.save_toggle, name="save_toggle"),
+
+    path("my_labels/", views.my_labels_view, name="my_labels"),
+    path("my_labels/create/", views.create_my_label, name="create_my_label")
 
 
     # path("newitem", views.newitem, name="newitem"),
--- dle/users/views.py
+++ dle/users/views.py
@@ -5,18 +5,19 @@ import decimal
 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.db import IntegrityError
+from django.db import IntegrityError, connection
 from django.core.paginator import Paginator
 # from .forms import CreateItemForm
-
+from .forms import MyLabelForm
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.utils import timezone
+import datetime as dt
+from django.core import management
+from .models import User, MyQueries, Post, MyLabel
 
-from .models import User, MyQueries, Post
-
-
+@login_required
 def index(request):
     return render(request, "users/index.html")
 
@@ -32,8 +33,7 @@ def login_view(request):
         # Check if authentication successful
         if user is not None:
             login(request, user)
-            #    return HttpResponseRedirect(reverse("index"))
-            return render(request, "users/index.html")
+            return redirect(reverse("users:index"))
 
         else:
             return render(
@@ -45,10 +45,10 @@ def login_view(request):
         return render(request, "users/login.html")
 
 
+@login_required
 def logout_view(request):
-    # returns user to login page
     logout(request)
-    return HttpResponseRedirect(reverse("login"))
+    return redirect(reverse("users:login"))
 
 
 def register(request):
@@ -73,8 +73,7 @@ def register(request):
                 request, "users/register.html", {"message": "Username already taken."}
             )
         login(request, user)
-        #    return HttpResponseRedirect(reverse("index"))
-        return render(request, "users/index.html")
+        return redirect(reverse("users:index"))
     else:
         return render(request, "users/register.html")
 
@@ -142,3 +141,81 @@ def profile(request, userid):
         "viewed_user": User.objects.get(pk=userid),
         "allposts": paginator.get_page(page)
     })        
+
+
+@login_required
+def my_labels_view(request, msg=None):
+    # create a blank form for the user to create a new my_label
+    form = MyLabelForm()
+    # get a list of the user's MyLabels
+    # note, could have performance issues if user has a "lot" of labels
+    my_labels = MyLabel.objects.filter(user=request.user).all()
+    context = {
+        "form": form,
+        "my_labels": my_labels,
+        "message": msg,
+    }
+    return render(request, "users/my_labels.html", context)
+
+
+@login_required
+def create_my_label(request):
+    """Accepts a request for a user to create a new MyLabel"""
+    if request.method == "POST":
+        form = MyLabelForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            file_name_suffix = form.cleaned_data["file"].name.split(".")[-1]
+            supported_filetypes = ["pdf", "xml"]
+            if file_name_suffix not in supported_filetypes:
+                msg = "File must be one of: " + str(supported_filetypes)
+                return my_labels_view(request, msg)
+
+            if form.cleaned_data["source"] == "EMA" and file_name_suffix != "pdf":
+                msg = "Only pdf files are supported for EMA drug labels"
+                return my_labels_view(request, msg)
+
+            if form.cleaned_data["source"] == "FDA" and file_name_suffix != "xml":
+                msg = "Only xml files are supported for FDA drug labels"
+                return my_labels_view(request, msg)
+
+            # create a DrugLabel object for the uploaded label
+            dl = DrugLabel(
+                source=form.cleaned_data["source"],
+                product_name=form.cleaned_data["product_name"],
+                generic_name=form.cleaned_data["generic_name"],
+                version_date=dt.datetime.now(),
+                source_product_number="my_label_" + form.cleaned_data["product_number"],
+                marketer=form.cleaned_data["marketer"],
+            )
+            try:
+                dl.save()
+            except IntegrityError:
+                msg = "Unable to add the drug label. Try changing the product number."
+                return my_labels_view(request, msg)
+
+            # create the MyLabel object referencing the DrugLabel
+            ml = MyLabel(
+                user=request.user,
+                drug_label=dl,
+                name=form.cleaned_data["name"],
+                file=request.FILES["file"],
+            )
+            ml.save()
+
+            # process the file
+            # send to load_fda_data or load_ema_data
+            # --type my_label --my_label_id ml.id
+            command = (
+                "load_fda_data"
+                if form.cleaned_data["source"] == "FDA"
+                else "load_ema_data"
+            )
+            management.call_command(command, type="my_label", my_label_id=ml.id)
+
+            # add to latest_drug_labels
+            sql = f"INSERT INTO latest_drug_labels VALUES ({dl.id})"
+            with connection.cursor() as cursor:
+                cursor.execute(sql)
+
+    return redirect(reverse("users:my_labels"))
