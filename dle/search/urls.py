d322704a04e5363e426978713fe7a4cf15ffb59e
llandau <leo_landau@yahoo.com>
2022-05-02 12:44:20 -0700

    cleanup a few unused files, functions

--- dle/search/urls.py
+++ dle/search/urls.py
@@ -7,5 +7,4 @@ app_name = "search"
 urlpatterns = [
     path("", views.index, name="index"),
     path("results", views.list_search_results, name="list_search_results"),
-    path("<int:drug_id>", views.view_drug, name="view_drug")
 ]
